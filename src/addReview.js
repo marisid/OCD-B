b000afb27fa5d8e77d3d97815a76543fe4d93a88
marisid <sid_marina@hotmail.com>
2016-12-01 15:26:02 +0000

    Get user id before inserting review to database

--- src/addReview.js
+++ src/addReview.js
@@ -1,9 +1,18 @@
 const dbConn = require('../db_connection');
 
 module.exports = (cb, params, user, resource) => {
-  let getUserId = `SELECT user_id FROM users WHERE username=${user}`
-  console.log('userId from database is ',getUserId);
-  dbConn.query(`INSERT INTO reviews (time_stamp, rating, summary, description, user_id) VALUES ($1, $2, $3, $4, ${getUserId} );`, [Date.now(), params.rating, params.summary_input, params.description_input], (error, data) => {
-    error ? cb(error) : cb(null);
+  let getUserId = 
+
+  dbConn.query(`SELECT user_id FROM users WHERE username=($1);`,[user], (error, data) => {
+    if(error) cb(error)
+    else {
+      let userId = data.rows[0].user_id;
+      console.log(userId);
+      dbConn.query(`INSERT INTO reviews (time_stamp, rating, summary, description, user_id) VALUES ($1, $2, $3, $4, $5 );`, [Date.now(), params.rating, params.summary_input, params.description_input, userId], (error, data) => {
+        error ? cb(error) : cb(null);
+      });
+    }
   });
+
+
 };

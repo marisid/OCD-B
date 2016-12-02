c165a96a1f610dce4fee1c7f1eec656255e1ee4b
msachi <marchee88@gmail.com>
2016-12-02 12:13:30 +0000

    Move routes.js to routes folder

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = [].concat(
-  require('./routes/login.js'),
-  require('./routes/register.js'),
-  require('./routes/dynamic_account.js'),
-  require('./routes/dynamic_addreview.js'),
-  require('./routes/dynamic_reviewform.js'),  
-);
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+module.exports = [].concat(
+  require('./login.js'),
+  require('./register.js'),
+  require('./dynamic_account.js'),
+  require('./dynamic_addreview.js'),
+  require('./dynamic_reviewform.js')  
+);

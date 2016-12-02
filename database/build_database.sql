0511b0ac6085aadff6b9dd00f9aa56660bda5eee
marisid <sid_marina@hotmail.com>
2016-12-02 12:35:27 +0000

    Change username and password data types to allow for more varying characters

--- database/build_database.sql
+++ database/build_database.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS users cascade;
 
 CREATE TABLE users (
   user_id   SERIAL    PRIMARY KEY   NOT NULL,
-  username    VARCHAR(15)   NOT NULL,
-  password    VARCHAR(15)   NOT NULL,
+  username    VARCHAR(100)   NOT NULL,
+  password    VARCHAR(50)   NOT NULL,
   experience    VARCHAR(10)
 );
 

566b10092905f567d8568be9f26e5057e520dd4d
marisid <sid_marina@hotmail.com>
2016-12-02 11:45:02 +0000

    Add two missing semicolons in the getData.js

--- src/getData.js
+++ src/getData.js
@@ -4,7 +4,7 @@ module.exports = {
   recentReviews: (cb, username) => {
     let reviewsUsersJoin =`SELECT * FROM reviews INNER JOIN users ON reviews.user_id = users.user_id`;
     let reviewsResourcesJoin = `INNER JOIN resources ON reviews.resource_id = resources.resource_id`;
-    dbConn.query(`${reviewsUsersJoin} ${reviewsResourcesJoin} WHERE username!=($1) ORDER BY time_stamp DESC LIMIT 3`,[username], (error, data) => {
+    dbConn.query(`${reviewsUsersJoin} ${reviewsResourcesJoin} WHERE username!=($1) ORDER BY time_stamp DESC LIMIT 3;`,[username], (error, data) => {
        error ? cb(error) : cb(null, data.rows);
     });
  },
@@ -16,7 +16,7 @@ module.exports = {
  myReviews: (cb, username) => {
    let userReviewsJoin =`SELECT * FROM reviews INNER JOIN users ON reviews.user_id = users.user_id`;
    let reviewsResourcesJoin = `INNER JOIN resources ON reviews.resource_id = resources.resource_id`;
-   dbConn.query(`${userReviewsJoin} ${reviewsResourcesJoin} WHERE username=($1) ORDER BY time_stamp DESC LIMIT 3`, [username], (error, data) => {
+   dbConn.query(`${userReviewsJoin} ${reviewsResourcesJoin} WHERE username=($1) ORDER BY time_stamp DESC LIMIT 3;`, [username], (error, data) => {
       error ? cb(error) : cb(null, data.rows);
    });
  }
